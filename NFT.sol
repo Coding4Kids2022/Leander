c7571f6ab426af06b847dfb9c7b74ad060f1ba6e
Snieboss <110459559+Snieboss@users.noreply.github.com>
2022-08-03 15:51:06 +0200

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -3,28 +3,27 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzepplin/contracts/untils/String.sol"
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
-
+     mapping(uint256 => uint) public NFTs_schon_gekauft;
     string public url;
 
-    constructor(string memory link) ERC721("NFTTutorial", "NFT") {
-    url = link
+    constructor(string memory link) ERC721(unicode"Schöne Bunte Farben", "Farbe") {
+    url = link;
     }
-
-    function mintTo(address recipient)
-        public
-        returns (uint256)
-    {
-        currentTokenId.increment();
-        uint256 newItemId = currentTokenId.current();
-        _safeMint(recipient, newItemId);
-        
-        function tokenURI(uint256 tokenId) override returns (string memory){
-             return string(abi.encodePacked(url, tokenID);
+    function mintTo(address recipient,uint256 pictur_number)
+        public payable
+    {   
+        require (msg.value>10 ether);
+        require (NFTs_schon_gekauft[pictur_number]<=50);
+        _safeMint(recipient, pictur_number);
+        NFTs_schon_gekauft[pictur_number]+=1;
+    }    
+        function tokenURI(uint256 tokenID) public view override returns (string memory) {
+             return string(abi.encodePacked(url, Strings.toString(tokenID),".json"));
         }
-    }
+    
 }
