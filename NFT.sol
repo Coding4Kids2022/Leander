a072c6e3cf855312af11a1753aa96a0a285d6066
Snieboss <110459559+Snieboss@users.noreply.github.com>
2022-08-04 10:18:45 +0200

    Update NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -11,19 +11,20 @@ contract NFT is ERC721 {
      mapping(uint256 => uint) public NFTs_schon_gekauft;
     string public url;
 
-    constructor(string memory link) ERC721(unicode"Schöne Bunte Farben", "Farbe") {
+    constructor(string memory link) ERC721(unicode"Schöne Bunte Farben", "Bunt") {
     url = link;
     }
     function mintTo(address recipient,uint256 pictur_number)
         public payable
     {   
-        require (msg.value>10 ether);
-        require (NFTs_schon_gekauft[pictur_number]<=50);
+        if (msg.value>10 ether) {
+            require (NFTs_schon_gekauft[pictur_number]<=50);
         _safeMint(recipient, pictur_number);
         NFTs_schon_gekauft[pictur_number]+=1;
-    }    
+        payable(0xd771a9bE526776B4855D69ED97c50da90C87ACf7).transfer(msg.value);
+        }  
+    }  
         function tokenURI(uint256 tokenID) public view override returns (string memory) {
              return string(abi.encodePacked(url, Strings.toString(tokenID),".json"));
         }
-    
 }
